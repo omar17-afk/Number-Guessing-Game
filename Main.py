a7530bfcc4a563afd3a2c7f0c5dedcf8cc7d7963
omar17-afk <omsmora2012@gmail.com>
2025-01-23 21:41:42 +0200

    play_game

--- Main.py
+++ Main.py
@@ -1,3 +1,14 @@
-import random
-def generate_random_number():
-    return random.randint(1, 100)
+def play_game():
+    print("Welcome to the Number Guessing Game!")
+    random_number = generate_random_number()
+    attempts = 0
+    while True:
+        guess = get_user_guess()
+        attempts += 1
+        if guess < random_number:
+            print("Too low! Try again.")
+        elif guess > random_number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You guessed the number in {attempts} attempts.")
+            break
